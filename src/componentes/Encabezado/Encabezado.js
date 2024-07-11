e46b2757b737f0c7e4bd215439e543f317ed81fb
Carla Blacio Reyes <carly.isa.0812@gmail.com>
2024-07-10 22:47:30 -0600

    creacionDeComponentes

---
+++ src/componentes/Encabezado/Encabezado.js
@@ -0,0 +1,10 @@
+import "./Encabezado.css"
+
+function Encabezado() {
+    return <header className="encabezado">
+        <img src='/img/header.png' alt='Org'/>
+    </header>
+    
+    
+}
+export default Encabezado
