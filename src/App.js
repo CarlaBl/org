8413814a42148765877c19fb92c3437c60becf25
Carla Blacio Reyes <carly.isa.0812@gmail.com>
2024-07-12 20:30:48 -0600

    funcionalidadDelFormulario

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
+import { useState } from 'react';
 import './App.css';
 import Encabezado from './componentes/Encabezado/Encabezado';
 import Formulario from './componentes/Formulario/Formulario';
+import MiOrg from './componentes/MiOrg';
 
 function App() {
+  const [mostrarFormulario, actualizarMostrar]= useState(true)
+
+  //Ternario --> condicion ? seMuestra : noSemuestra
+  // condicion && seMuestra
+
+  const cambiarMostrar = () =>{
+    actualizarMostrar(!mostrarFormulario)
+  }
+
   return (
     <div>
       <Encabezado/>
-      <Formulario/>
-      
-      
+      {/*mostrarFormulario === true ? <Formulario/> : <div></div>*/}
+      {mostrarFormulario && <Formulario/> }
+      <MiOrg cambiarMostrar={cambiarMostrar} />
     </div>
   );
 }
--- src/componentes/Formulario/Formulario.js
+++ src/componentes/Formulario/Formulario.js
@@ -1,24 +1,58 @@
+import { useState } from "react"
 import "./Formulario.css"
 import CampoTexto from "../CampoTexto"
 import ListaOpciones from "../ListaOpciones"
 import Boton from "../Boton"
 
 const Formulario = () =>{
+    const [nombre,actualizarNombre]= useState("")
+    const [puesto,actualizarPuesto]= useState("")
+    const [foto,actualizarFoto]= useState("")
+    const [equipo,actualizarEquipo]= useState("")
 
     const manenjarEnvio = (e)=>{
         e.preventDefault()
-        console.log("Manejar envio", e);
+        console.log("Manejar envio");
+        let datosEnviar = {
+            nombre:nombre,
+            puesto:puesto,
+            foto:foto,
+            equipo:equipo
+        }
+        console.log(datosEnviar);
     }
-
 
     return <section className="formulario">
         <form onSubmit={manenjarEnvio}>
             <h2>Rellena el formulario para crear el colaborador</h2>
-            <CampoTexto titulo="Nombre" placeholder="Ingresar nombre" required/>
-            <CampoTexto titulo="Puesto" placeholder="Ingresar puesto" required/>
-            <CampoTexto titulo="Foto" placeholder="Ingresar enlace de foto" required/>
-            <ListaOpciones/>
-            <Boton>Crear</Boton>
+            <CampoTexto 
+                titulo="Nombre" 
+                placeholder="Ingresar nombre" 
+                required 
+                valor={nombre} 
+                actualizarValor={actualizarNombre}
+                />
+            <CampoTexto 
+                titulo="Puesto" 
+                placeholder="Ingresar puesto" 
+                required 
+                valor={puesto}
+                actualizarValor={actualizarPuesto}
+                />
+            <CampoTexto 
+                titulo="Foto" 
+                placeholder="Ingresar enlace de foto" 
+                required 
+                valor={foto}
+                actualizarValor={actualizarFoto}
+                />
+            <ListaOpciones
+                valor={equipo}
+                actualizarValor={actualizarEquipo}
+                />
+            <Boton>
+                Crear
+            </Boton>
         </form>
     </section>
 }
